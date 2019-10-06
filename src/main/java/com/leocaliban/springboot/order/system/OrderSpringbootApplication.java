cc4888a3d6b8ba2bf9baebaa5f28dff45339884b
Leocaliban <leocaliban@gmail.com>
2019-10-06 18:44:35 -0300

    Create project

---
+++ src/main/java/com/leocaliban/springboot/order/system/OrderSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.leocaliban.springboot.order.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSpringbootApplication.class, args);
+	}
+
+}
