b79f31e7e2b7f31bfd31d7be4f302494752473d2
Leocaliban <leocaliban@gmail.com>
2019-10-06 19:36:45 -0300

    Create Service layer and add component registration

--- src/main/java/com/leocaliban/springboot/order/system/controllers/UserController.java
+++ src/main/java/com/leocaliban/springboot/order/system/controllers/UserController.java
@@ -1,19 +1,33 @@
 package com.leocaliban.springboot.order.system.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leocaliban.springboot.order.system.entities.User;
+import com.leocaliban.springboot.order.system.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> getAll() {
-		User u = User.builder().id(1L).name("Jack").password("6876").phone("95989599898").build();
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> getAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> getById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/leocaliban/springboot/order/system/repositories/UserRepository.java
+++ src/main/java/com/leocaliban/springboot/order/system/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.leocaliban.springboot.order.system.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.leocaliban.springboot.order.system.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
